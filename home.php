d988a49d08f2d98b17c3080ad7883c9bb0f60736
Ameynuddin <md.ameynuddin@gmail.com>
2024-08-21 12:35:10 +0800

    added footer

--- home.php
+++ home.php
@@ -25,7 +25,45 @@
 
         <i id="menu-btn" class="fas fa-bars"></i>
     </section>
-    
+
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>Quick Links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"> Home</i></a>
+                <a href="about.php"><i class="fas fa-angle-right"> About</i></a>
+                <a href="package.php"><i class="fas fa-angle-right"> Package</i></a>
+                <a href="book.php"><i class="fas fa-angle-right"> Book</i></a>
+            </div>
+
+            <div class="box">
+                <h3>Extra Links</h3>
+                <a href="#"><i class="fas fa-angle-right"> Ask Questions</i></a>
+                <a href="#"><i class="fas fa-angle-right"> About Us</i></a>
+                <a href="#"><i class="fas fa-angle-right"> Privacy Policy</i></a>
+                <a href="#"><i class="fas fa-angle-right"> Terms of Use</i></a>
+            </div>
+
+            <div class="box">
+                <h3>Contact Info</h3>
+                <a href="#"><i class="fas fa-phone"> +1234556789</i></a>
+                <a href="#"><i class="fas fa-phone"> +0123456789</i></a>
+                <a href="#"><i class="fas fa-envelope"> Privacy Policy</i></a>
+                <a href="#"><i class="fas fa-map"> Sabah, Malaysia</i></a>
+            </div>
+
+            <div class="box">
+                <h3>Follow Us</h3>
+                <a href="#"><i class="fab fa-facebook"> Facebook</i></a>
+                <a href="#"><i class="fab fa-twitter"> Twitter</i></a>
+                <a href="#"><i class="fab fa-instagram"> Instagram</i></a>
+                <a href="#"><i class="fab fa-linkedin"> LinkedIn</i></a>
+            </div>
+        </div>
+
+
+    </section>
+
     <script src="js/script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 </body>
