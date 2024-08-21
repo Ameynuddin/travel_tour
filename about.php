8f2079c1dd45442153d4a4ea5dbfc2f35f6ea2b0
Ameynuddin <md.ameynuddin@gmail.com>
2024-08-21 13:46:59 +0800

    added about page

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>About</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
@@ -25,6 +25,11 @@
 
         <i id="menu-btn" class="fas fa-bars"></i>
     </section>
+
+    <!-- content -->
+    <div class="heading" style="background:url(images/header-bg-1.jpg) no-repeat">
+        <h1>About Us</h1>
+    </div>
 
     <section class="footer">
         <div class="box-container">
